5a61f7a38bfdfc7edaac151ea5b85bc0d44b7dcc
dvnkboi <50340902+dvnkboi@users.noreply.github.com>
2021-04-26 13:50:15 +0000

    db exports

--- Run.ps1
+++ Run.ps1
@@ -1,2 +1,2 @@
 New-Item -ItemType Directory -Force -Path Logs
-npm start | Out-File -FilePath ("Logs\\" + (Get-Date).ToUniversalTime().ToString("yyyyMMdd-HHmmss") + ".txt")
+node . 3>&1 2>&1 | Out-File -FilePath ("Logs\\" + (Get-Date).ToUniversalTime().ToString("yyyyMMdd-HHmmss") + ".txt")
